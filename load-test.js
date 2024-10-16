83fa5a1b38b6622d411105a871b6dbcfed1f8ee4
ngtianxun08 <e1327862@u.nus.edu>
2024-10-16 13:54:15 +0800

    * Added HPA for all services.

--- load-test.js
+++ load-test.js
@@ -3,13 +3,13 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 50 }, // Ramp up
-    { duration: '5m', target: 50 }, // Sustain
+    { duration: '1m', target: 50 }, // Ramp up
+    { duration: '3m', target: 50 }, // Sustain
     { duration: '2m', target: 0 },  // Ramp down
   ],
 };
 
 export default function () {
-  http.get('http://crypto.local/api/v1/notifications/health');
+  http.get('http://34.160.254.13/api/v1/notifications/health');
   sleep(1);
 }
